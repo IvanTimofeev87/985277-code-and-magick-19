bca3e30436931684b270c791011f560e34fe2c54
Timofeev Ivan <ivant87@mail.ru>
2020-02-28 08:06:09 +0300

    продолжение задания орды

--- js/setup.js
+++ js/setup.js
@@ -4,15 +4,10 @@
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLOR = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
-var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green']
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var SAME_WIZARD_ARRAY = makeSameWizardArray();
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
+/*Функции*/
 
 function getRandomNumb(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -22,44 +17,40 @@ function getRandomItem(array) {
   return array[getRandomNumb(0, array.length)];
 };
 
-var wizards = [{
-    name: WIZARD_NAMES[0] + ' ' + WIZARD_SURNAMES[0],
-    coatColor: getRandomItem(WIZARD_COAT_COLOR),
-    eyesColor: getRandomItem(WIZARD_EYES_COLOR)
-  },
-  {
-    name: WIZARD_NAMES[1] + ' ' + WIZARD_SURNAMES[1],
-    coatColor: getRandomItem(WIZARD_COAT_COLOR),
-    eyesColor: getRandomItem(WIZARD_EYES_COLOR)
-  },
-  {
-    name: WIZARD_NAMES[2] + ' ' + WIZARD_SURNAMES[2],
-    coatColor: getRandomItem(WIZARD_COAT_COLOR),
-    eyesColor: getRandomItem(WIZARD_EYES_COLOR)
-  },
-  {
-    name: WIZARD_NAMES[3] + ' ' + WIZARD_SURNAMES[3],
-    coatColor: getRandomItem(WIZARD_COAT_COLOR),
-    eyesColor: getRandomItem(WIZARD_EYES_COLOR)
+function getSameWizard(idx) {
+  return {
+    wizard: {
+      name: WIZARD_NAMES[idx] + ' ' + WIZARD_SURNAMES[idx],
+      coatColor: getRandomItem(WIZARD_COAT_COLOR),
+      eyesColor: getRandomItem(WIZARD_EYES_COLOR)
+    }
   }
-];
+};
 
-/*Функции*/
+function makeSameWizardArray() {
+  var array = [];
+  for (var i = 0; i < 4; i++) {
+    array.push(getSameWizard(i));
+  }
+  return array;
+};
 
-var renderWizard = function(wizard) {
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+for (var i = 0; i < SAME_WIZARD_ARRAY.length; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
-  return wizardElement;
+  wizardElement.querySelector('.setup-similar-label').textContent = SAME_WIZARD_ARRAY[i].wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = SAME_WIZARD_ARRAY[i].wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = SAME_WIZARD_ARRAY[i].wizard.eyesColor;
+  similarListElement.appendChild(wizardElement);
 }
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
