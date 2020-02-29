3a6e36cf251f82953fe9d65727c2faf75a77215f
Timofeev Ivan <ivant87@mail.ru>
2020-02-29 19:00:55 +0300

    сделал задание орды

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,7 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var SAME_WIZARD_ARRAY = makeSameWizardArray();
 
@@ -38,19 +38,23 @@ function makeSameWizardArray() {
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+function renderWizard() {
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
 
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
 
-for (var i = 0; i < SAME_WIZARD_ARRAY.length; i++) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = SAME_WIZARD_ARRAY[i].wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = SAME_WIZARD_ARRAY[i].wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = SAME_WIZARD_ARRAY[i].wizard.eyesColor;
-  similarListElement.appendChild(wizardElement);
-}
+  for (var i = 0; i < SAME_WIZARD_ARRAY.length; i++) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = SAME_WIZARD_ARRAY[i].wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = SAME_WIZARD_ARRAY[i].wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = SAME_WIZARD_ARRAY[i].wizard.eyesColor;
+    similarListElement.appendChild(wizardElement);
+  }
 
+};
+
+renderWizard();
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
